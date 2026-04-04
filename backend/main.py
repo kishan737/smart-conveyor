3869cbfa69ad09bf7cd671d9c4b7316f33f0175b
kishan737 <kishan182005@gmail.com>
2026-04-04 09:05:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://smart-conveyor.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
